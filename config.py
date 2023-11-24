aa237bc574ee2c220437a4d6a87cf4bc67516a05
isharkbd <138902966+isharkbd@users.noreply.github.com>
2023-11-25 01:21:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("29838522"))
-API_HASH = getenv("ea0003cc221afe6e0ca253c091745703")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("6874503477:AAFRxS9pmHVBSV1Jm24eo5Hbg4GZSEO4Px4", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("6368506857"))
+OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/7d201555feafaca3b18fe.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/de126268f8cef53390434.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("BQHAb8AAVjI_nXDy1KD94QTC4xolDP5t5xSDIF9Qy7Pnlkih8akIvt2pbqeCFdFuEAKalDwK65YGFYzB9QbOrGI8yqmTuseWNSJrl6VlLFwVzaSCTR2rfgcFBhqfTHH8SDWiadLM50DIoxTsl80vIMC121_o9pm8w2pQ2v75bAbt9y7wwChGqh8Kj5SEp7bTyuu4LQi3aTT412JVo28tTDYUz-J7lde2AkiICPY810f5uIY4GHNo6wfiJSanhBlxdX8G3LcYBJacG4IHJFaVG-xk2NX4TxPYMtliJCTHQRchJU6uEjKc9W0eFUTtPAt9tVO5Rno7lSpBE5sVG7lJ6GmIcpbvHAAAAAFAK8reAA", None)
+SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nmb_xD")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NoMoreBins")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5371579102").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://graph.org/file/51ac174f20b91e941b318.jpg"
+FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
